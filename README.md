5902d627df2dd47db8bf029ab1dee820cd9cf37c
Evans Nyarko <evans.nyarko39@yahoo.com>
2024-07-02 22:56:13 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Files Manager Project
+
+## Description
+
+The objective is to build a simple platform to upload and view files:
+
+- User authentication via a token
+- List all files
+- Upload a new file
+- Change permission of a file
+- View a file
+- Generate thumbnails for images
---
+++ utils/redis.js
@@ -0,0 +1,33 @@
+import { promisify } from 'util';
+import { createClient } from 'redis';
+
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+    this.client.on('error', (err) => {
+      console.log(`Redis client not connected to the server: ${err.message}`);
+    });
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    const getAsync = promisify(this.client.get).bind(this.client);
+    return getAsync(key);
+  }
+
+  async set(key, value, duration) {
+    const setAsync = promisify(this.client.set).bind(this.client);
+    await setAsync(key, value, 'EX', duration);
+  }
+
+  async del(key) {
+    this.client.del(key);
+  }
+}
+
+const redisClient = new RedisClient();
+
+export default redisClient;
