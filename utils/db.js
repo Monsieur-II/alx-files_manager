fd2440bea2c5b7b7f737d33c9c10ec3c27c7be92
Evans Nyarko <evans.nyarko39@yahoo.com>
2024-07-03 01:13:36 +0000

    bug fix

--- utils/db.js
+++ utils/db.js
@@ -1,7 +1,5 @@
 import { MongoClient } from 'mongodb';
-import dotenv from 'dotenv';
 
-dotenv.config();
 const host = process.env.DB_HOST || 'localhost';
 const port = process.env.DB_PORT || '27017';
 const dbName = process.env.DB_DATABASE || 'files_manager';
@@ -10,7 +8,6 @@ const uri = `mongodb://${host}:${port}/${dbName}`;
 class DBClient {
   constructor() {
     this.client = new MongoClient(uri, {
-      useNewUrlParser: true,
       useUnifiedTopology: true,
     });
     this.client.connect();
@@ -21,11 +18,11 @@ class DBClient {
   }
 
   async nbUsers() {
-    return this.client.db(dbName).collection('users').countDocuments();
+    return this.client.db().collection('users').countDocuments();
   }
 
   async nbFiles() {
-    return this.client.db(dbName).collection('files').countDocuments();
+    return this.client.db().collection('files').countDocuments();
   }
 }
 
